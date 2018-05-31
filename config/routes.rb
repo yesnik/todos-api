ceefbbd1ebd93470eab1b50727cc8a4906a10352
Nikita Nyatin <yesnik@yandex.ru>
2018-05-31 09:26:40 +0500

    Add api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
+  # module the controllers without affecting the URI
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
+  # Remember, non-default versions have to be defined above the default version.
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
